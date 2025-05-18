6a95b70cfd42e1f8f365bd51bc39710176c0e2fb
katmunkova <katmunkova@gmail.com>
2025-05-18 12:47:33 +0300

    Ввела в игру уровень

---
+++ hangman.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("Добро пожаловать в игру")
+
+
+print("Введи своё имя")
+player_name = input()
+
+print(f"Приветствую тебя, {player_name}")
+
+print(f"Теперь, {player_name}, выбери уровень сложности.")
+
+print(f" Для этого введи одну из 3х цифр: 1, 2, 3. 1 - лёгкий уровень, 2 - средний, 3 - сложный")
+
+level = int(input())
+
