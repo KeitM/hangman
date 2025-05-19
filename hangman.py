fba7cff9aa6bb0a3af2c8128aeeb2459bf1137e8
katmunkova <katmunkova@gmail.com>
2025-05-19 23:00:15 +0300

    Добавила пару фкнкций уровней

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
 import os
+import random
+import linecache
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -12,18 +14,51 @@ print(f"Приветствую тебя, {player_name}")
 
 print(f"Теперь, {player_name}, выбери уровень сложности.")
 
-print(f" Для этого введи одну из 3х цифр: 1, 2, 3. 1 - лёгкий уровень, 2 - средний, 3 - сложный")
+print(f"Для этого введи одну из 3х цифр: 1, 2, 3. 1 - лёгкий уровень, 2 - средний, 3 - сложный")
 
 level = int(input())
 
-# if level == 1:
-   # first_level():
-# if level == 2:
-# if level == 3:
-# else:
-#    print("Введите крректные данные")
 
 def first_level():
-    with open('example.txt', 'r') as file:
-    content = file.readline()
-    print(content)
+   with open('words_easy.txt', 'r', encoding="utf-8") as file:
+      line_number = random.randint(1,5)
+      desired_line = linecache.getline('words_easy.txt', line_number).strip().split(":")
+      text = desired_line[1:]
+      word_for_guessing = desired_line[0]
+   print(f"Отгадай загаданное слово. Что это может быть:{' '.join(text)}") 
+   
+   return word_for_guessing
+
+def second_level():
+   with open('words_medium.txt', 'r', encoding="utf-8") as file:
+      line_number = random.randint(1,10)
+      desired_line = linecache.getline('words_medium.txt', line_number).strip().split(":")
+      text = desired_line[1:]
+      word_for_guessing = desired_line[0]
+   print(f"Отгадай загаданное слово. Что это может быть:{' '.join(text)}") 
+   
+   return word_for_guessing
+
+def human_answers(word_for_guessing):
+   letters_for_guessing = list(word_for_guessing)
+   
+   while word_for_guessing != str(letters_for_guessing):
+      print("Введи букву загаданного слова") 
+      letter = input()
+      
+   for i in range(0,len(letters_for_guessing)):
+      if letter == letters_for_guessing[i]:
+         with open('responses', 'r', encoding="utf-8") as file:
+            desired_line2 = linecache.getline('responses.txt', 1).strip().split(":")
+            text = desired_line2[1:]
+            
+   print(f"{' '.join(text)}")
+      
+
+if level == 1:
+   first_level()
+elif level == 2:
+   second_level()
+# if level == 3:
+else:
+   print("Введите крректные данные")
